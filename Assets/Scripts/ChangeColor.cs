1d325d253f894b1a8425e3a15c12d67f99a8af12
DieMusterloesung <christoph.tschechne@protonmail.com>
2020-03-27 20:06:59 +0100

    Added Triangle, ChangeColor

---
+++ Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    public Color[] colors;
+    internal int currentColor = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            GetComponent<SpriteRenderer>().color = colors[currentColor];
+            currentColor++;
+            if (currentColor == colors.Length) currentColor = 0;
+        }
+    }
+
+
+}
